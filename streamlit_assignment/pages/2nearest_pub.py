8e4f363d3bd7aa72f9a3ce5ee755ae1135a0622c
indramanishukla <106675593+indramanishukla@users.noreply.github.com>
2022-08-28 00:46:12 +0530

    Add files via upload

---
+++ streamlit_assignment/pages/2nearest_pub.py
@@ -0,0 +1,51 @@
+from sre_parse import expand_template
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from math import radians, cos, sin, asin, sqrt
+import haversine as hs
+
+
+st.title('Pubs near me!')
+st.write('This is section where you can search pubs near you, give your coordinates.')
+df = pd.read_csv("C:/Users/Indramani Shukla/Downloads/Innomatics Internship/Streamlit/streamlit_assignment/open_pubs.csv", header=None, names=['fsa_id','name','address','postcode','easting','northing','latitude','longitude','local_authority'])
+df.latitude.replace('\\N', np.nan, inplace=True)
+df.longitude.replace('\\N', np.nan, inplace=True)
+df.dropna(inplace=True)
+
+
+df.latitude = df.latitude.astype('float64')
+df.longitude = df.longitude.astype('float64')
+df['city'] = [i[-1] for i in df.address.str.split()]
+
+in_col, out_col = st.columns(2)
+
+lat = in_col.number_input('Enter your latitude in Britain', 51.50)
+lon = in_col.number_input('Enter your longitude in Britain', 0.12)
+find = pd.DataFrame([[lat, lon]], columns=['lat', 'lon'])
+out_col.text('Here you are located!')
+out_col.map(find)
+
+
+dist =[]
+
+df.reset_index(drop=True, inplace=True)
+
+
+def nearest(latitude, lat, longitude, lon):
+    near =((df['latitude']-lat)**2+(df['longitude']-lon)**2)**0.5
+
+    return near
+
+
+
+df['nearest_eucledian_dist'] = nearest(df.latitude, lat, df.longitude, lon)
+data = df.sort_values('nearest_eucledian_dist', ascending=True).head()
+st.write(data)
+st.text('Five Pubs Nearest to You, Cheers!!!')
+st.map(data)
+
+# st.write(dist)
